ed002f52abe7617c24bd1622101a66a4baf7a8d3
issam <issam@RPC-2RVWMQ2>
2025-02-02 19:28:57 -0500

    Push to Remote Repository.

--- src/a/ADemo.java
+++ src/a/ADemo.java
@@ -4,6 +4,8 @@ public class ADemo {
 	
 	public static void main(String[] args) {
 		System.out.println("ADemo: TeamA");
+		
+		System.out.println("Part 2 of the homework - Push from Local To remote Repository");
 	}
 
 }
