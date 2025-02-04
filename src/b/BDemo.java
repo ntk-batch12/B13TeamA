909d15d089d9d1604baae9964f0d741e669c90e5
Farah <Farah@DESKTOP-4IKORCG.lan>
2025-02-03 19:33:38 -0600

    we're testing

--- src/b/BDemo.java
+++ src/b/BDemo.java
@@ -5,5 +5,7 @@ public class BDemo {
 	public static void main(String[] args) {
 		System.out.println("BDemo: TeamA");
 	}
-
+	public static void createTean() {
+		System.out.println("TeamA");
+	}
 }
