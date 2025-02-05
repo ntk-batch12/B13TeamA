f6aebb6df821690264cbb20980bc36aa305589f5
salsh <salsh@Sally.lan>
2025-02-05 02:54:56 -0500

    adding getColor() -- by Sally

--- src/a/ADemo.java
+++ src/a/ADemo.java
@@ -6,8 +6,9 @@ public class ADemo {
 		System.out.println("ADemo: TeamA");
 
 		System.out.println("cool kids club");
-			String 	name = "Sally";
-			//test test
+		String name = "Sally";
+		System.out.println(name);
+		// test test
 		for (int i = 0; i >= 20; i++) {
 
 			System.out.println(i);
@@ -26,69 +27,65 @@ public class ADemo {
 
 		}
 
-}
-	
+	}
 
-	}
-	
-	for(int y = 0; y >= 25; y++) {
-		System.out.println(y);
-		
-	}
-	
-	
 	public static int add(int a, int b) {
 		int result = 0;
 		result = a + b;
 		return result;
-}
+	}
+
 	public void greeting() {
 		System.out.println("Hello teamA");
+	}
+
 	public String comboString(String a, String b) {
-		  String result = "";
-		  
-		  int lengtha = a.length();
-		  int lengthb = b.length();
-		  
-		  if(lengtha < lengthb){
-		    result = a + b + a;
-		  } else{
-		    result = b + a + b;
-		  }
-		  
-		  return result;
+		String result = "";
+
+		int lengtha = a.length();
+		int lengthb = b.length();
+
+		if (lengtha < lengthb) {
+			result = a + b + a;
+		} else {
+			result = b + a + b;
 		}
-	
+
+		return result;
+	}
+
 	// New Method - Sam
 	public boolean sleepIn(boolean weekday, boolean vacation) {
-		  
-		  boolean result = false;
-		  
-		  if (!weekday || vacation == true) {
-		    result = true;
-		  } 
-		    return result;
 
+		boolean result = false;
+
+		if (!weekday || vacation == true) {
+			result = true;
 		}
-	}
-}
+		return result;
 
-}
+	}
+
 	public void greating() {
 		System.out.println("hello teamA");
 	}
-	//new method by Farah
-	
+	// new method by Farah
+
 	public int sumDouble(int a, int b) {
 		int sum = a + b;
-		
+
 		if (a == b) {
 			sum = sum * 2;
 		}
-		return sum
+		return sum;
 	}
-	
-	//git switch main
-	   // git merge new-joke
+
+	//new method by sally
+	public static void getColor(String color) {
 		
+	 System.out.println("the color of the season: " + color);
+	 
+	
 	}
+
+}
