36563f438e90563cee699391d1f34cd63dd04d25
salsh <salsh@Sally.lan>
2025-02-04 20:25:45 -0500

    fixing the errors

--- src/a/ADemo.java
+++ src/a/ADemo.java
@@ -1,41 +1,29 @@
 package a;
 
 public class ADemo {
-	
+
 	public static void main(String[] args) {
 		System.out.println("ADemo: TeamA");
-		
+
 		System.out.println("cool kids club");
-		
+
 		for (int i = 0; i >= 20; i++) {
-			
+
 			System.out.println(i);
 		}
-	}
-	if (goal.isChallenging()) {
-	    System.out.println("Keep pushing, you're doing great!");
-	    goal.moveForward();
-	
-	
-int a=5;
-int b= 10;
 
-System.out.println(a+b);
-}
+		int a = 5;
+		int b = 10;
 
-}
-	
+		if (a != b) {
 
-	}
-	
-	for(int y = 0; y >= 25; y++) {
-		System.out.println(y);
-		
-	}
-	}
-	
-	for(int y = 0; y >= 25; y++) {
-		System.out.println(y);
-		
+			System.out.println("Keep pushing, you're doing great!");
+		}
+
+		for (int y = 0; y >= 25; y++) {
+			System.out.println(y);
+
+		}
+
 	}
 }
