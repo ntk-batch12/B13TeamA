a1ca046f72d0116093d0962618945e892ee28ec9
mdaea <mdaea@mdaea.sacm.org>
2025-02-05 14:26:51 -0500

    added new methods for ADemo & BDemo classes

--- src/a/ADemo.java
+++ src/a/ADemo.java
@@ -1,41 +1,22 @@
 package a;
 
 public class ADemo {
-	
+
 	public static void main(String[] args) {
-		System.out.println("ADemo: TeamA");
-		
-		System.out.println("cool kids club");
-		
-		for (int i = 0; i >= 20; i++) {
-			
-			System.out.println(i);
-		}
-	}
-	if (goal.isChallenging()) {
-	    System.out.println("Keep pushing, you're doing great!");
-	    goal.moveForward();
-	
-	
-int a=5;
-int b= 10;
-
-System.out.println(a+b);
-}
-
-}
-	
+		System.out.println("Branch Dev:XYZ");
 
 	}
-	
-	for(int y = 0; y >= 25; y++) {
-		System.out.println(y);
-		
+
+	public void add() {
+		System.out.println(1 + 2);
+
 	}
+
+	public void divide() {
+		System.out.println(1 / 2);
 	}
-	
-	for(int y = 0; y >= 25; y++) {
-		System.out.println(y);
-		
+
+	public void getData() {
+		System.out.println("Test");
 	}
 }
--- src/b/BDemo.java
+++ src/b/BDemo.java
@@ -5,5 +5,8 @@ public class BDemo {
 	public static void main(String[] args) {
 		System.out.println("BDemo: TeamA");
 	}
+public void setCountry() {
+	System.out.println("USA");
+}
 
 }
