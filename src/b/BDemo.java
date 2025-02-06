cbebdb2e4a8f3f68d096dc2780f427ff2bad6639
salsh <salsh@Sally.lan>
2025-02-06 18:44:17 -0500

    fixing the errors

--- src/b/BDemo.java
+++ src/b/BDemo.java
@@ -1,31 +1,27 @@
 package b;
 
 public class BDemo {
-	
+
 	public static void main(String[] args) {
 		System.out.println("BDemo: TeamA");
-	} 
+		int a = 5;
+		int b = 10;
+		int sum = a + b;
+		System.out.println("The sum of " + a + "and " + b + " is: " + sum);
+
+		// new code was not good @ Team
+	}
+
 	String Name = "Abdullah";
 	String job = "QA Eng.";
-	
-	public void setJob() {
-	System.out.println(Name + "is a " + job);
-	
 
-		
-		// Variables
-		int a = 20;
-    int b = 40;
-    
+	public void setJob() {
+		System.out.println(Name + "is a " + job);
+
+	}
+
 	public static void createTeam() {
 		System.out.println("TeamA");
 	}
-	int a = 5;
-	int b= 10;
-	int sum = a + b;
-	System.out.println("The som of " + a + "and " + b + " is: " + sum);
-    
-// new code was not good @ Team
-
 
 }
