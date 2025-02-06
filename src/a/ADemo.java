e6a203b5b456b6689661c777f3f9ff42ecebe7b5
Farah <Farah@DESKTOP-4IKORCG.lan>
2025-02-05 22:27:06 -0600

    Merge branch 'farah' of https://github.com/ntk-batch12/B13TeamA.git into farah

--- src/a/ADemo.java
+++ src/a/ADemo.java
@@ -11,6 +11,7 @@ public class ADemo {
 			
 			System.out.println(i);
 		}
+<<<<<<< HEAD
 		
 		//calling setGoal method
 		ADemo demo = new ADemo();
@@ -27,10 +28,27 @@ public class ADemo {
 	
 int a=5;
 int b= 10;
+=======
+>>>>>>> branch 'farah' of https://github.com/ntk-batch12/B13TeamA.git
 
+<<<<<<< HEAD
 System.out.println("The sum is: " + (a+b));
 
 
+=======
+		int a = 5;
+		int b = 10;
+
+		if (a != b) {
+
+			System.out.println("Keep pushing, you're doing great!");
+		}
+
+		for (int y = 0; y >= 25; y++) {
+			System.out.println(y);
+
+		}
+>>>>>>> branch 'farah' of https://github.com/ntk-batch12/B13TeamA.git
 
 }
 	
@@ -41,15 +59,46 @@ System.out.println("The sum is: " + (a+b));
 		System.out.println(y);
 		
 	}
-	}
 	
-	for(int y = 0; y >= 25; y++) {
-		System.out.println(y);
-		
+	
+	public static int add(int a, int b) {
+		int result = 0;
+		result = a + b;
+		return result;
+}
+	public void greeting() {
+		System.out.println("Hello teamA");
+	public String comboString(String a, String b) {
+		  String result = "";
+		  
+		  int lengtha = a.length();
+		  int lengthb = b.length();
+		  
+		  if(lengtha < lengthb){
+		    result = a + b + a;
+		  } else{
+		    result = b + a + b;
+		  }
+		  
+		  return result;
+		}
+	
+	// New Method - Sam
+	public boolean sleepIn(boolean weekday, boolean vacation) {
+		  
+		  boolean result = false;
+		  
+		  if (!weekday || vacation == true) {
+		    result = true;
+		  } 
+		    return result;
+
+		}
 	}
 	public void setMode() {
 		System.out.println("Happy!");
 }
+<<<<<<< HEAD
 	public void setGoal() {
 		System.out.println("Blast off to the moon!");
 	
@@ -57,3 +106,29 @@ System.out.println("The sum is: " + (a+b));
 	System.out.println("Good Morning teamA");
 	}
 }
+=======
+
+}
+	public void greating() {
+		System.out.println("hello teamA");
+	}
+	//new method by Farah
+	
+	public int sumDouble(int a, int b) {
+		int sum = a + b;
+		
+		if (a == b) {
+			sum = sum * 2;
+		}
+		return sum
+	}
+	
+	System.out.println("Hi");
+	
+	//git switch main
+	   // git merge new-joke
+		
+	}
+>>>>>>> branch 'farah' of https://github.com/ntk-batch12/B13TeamA.git
+	
+	
--- src/b/BDemo.java
+++ src/b/BDemo.java
@@ -4,14 +4,32 @@ public class BDemo {
 	
 	public static void main(String[] args) {
 		System.out.println("BDemo: TeamA");
+<<<<<<< HEAD
 	
 	BDemo demo = new BDemo();
 	demo.createTeam();
 	
+=======
+		
+		// Variables
+		int a = 20;
+    int b = 40;
+    
+	public static void createTeam() {
+		System.out.println("TeamA");
+	}
+>>>>>>> branch 'farah' of https://github.com/ntk-batch12/B13TeamA.git
 	int a = 5;
 	int b= 10;
 	int sum = a + b;
+<<<<<<< HEAD
 	System.out.println("The sum of " + a + "and " + b + " is: " + sum);
+=======
+	System.out.println("The som of " + a + "and " + b + " is: " + sum);
+    
+// new code was not good @ Team
+
+>>>>>>> branch 'farah' of https://github.com/ntk-batch12/B13TeamA.git
 }
 	public void createTeam() {
 		System.out.println("TeamA");
