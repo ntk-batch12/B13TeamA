4d1c1cf02a98478742302bef5327e2c5a37ad76e
issam <issam@RPC-2RVWMQ2>
2025-02-07 20:11:51 -0500

    Adding For Loop.

--- src/CDemo.java
+++ src/CDemo.java
@@ -1,40 +1,50 @@
+import java.util.Scanner;
 
 public class CDemo {
 	
 	
 	public static void main (String args[]) {
 		
+		// For Loop - Time Table
+	
+		Scanner sc = new Scanner(System.in);
+		
+		// Ask the user to Enter a Number
+		System.out.println("Please Enter a Number: ");
+		int m = sc.nextInt();
+		
+		// For Loop
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(m + " X " + i + " = " + m * i);
+		}
+		
+		
+
 		int a = 15;
-		int b= 20;
+		int b = 20;
 
-		
 		String team = "Team1";
-		
-		
-
-
 
 		int c = 30;
 		System.out.println(c);
 		String school = "USF";
 		System.out.println(school);
 
-		 a = 3;
+		a = 3;
 		int d = 2;
-			int j = 4;
+		int j = 4;
 
-			
-			String name = "Abdullah";
-			
-			int f = 9;
+		String name = "Abdullah";
+
+		int f = 9;
 
 		System.out.println("create a conflict");
 
 	}
-		public void add() {
-			System.out.println(1 + 2);
 
-			
+	public void add() {
+		System.out.println(1 + 2);
+
 		
 	}
 
